ae328851da90f6441f432a8ce625e42e7c91576a
rong fengliang <1141591465@qq.com>
2020-08-26 20:11:15 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import demo from "src/main/resources/demo2.mjs"
+import demo from "https://raw.githubusercontent.com/rongfengliang/graalvm-js-learning/master/demo.js"
 let info  = demo()
 console.log("-----js-------")
 console.log(info)
